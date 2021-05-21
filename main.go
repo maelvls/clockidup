0a2737c027d9e490202e544487992adefd38f787
Maël Valais <mael@vls.dev>
2021-05-21 18:46:14 +0200

    add the "help" command

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"strings"
+	"text/template"
 	"time"
 
 	"github.com/MakeNowJust/heredoc/v2"
+	"github.com/mgutz/ansi"
 	"github.com/tj/go-naturaldate"
 
 	"github.com/maelvls/clockidup/logutil"
@@ -34,37 +37,114 @@ var (
 	date    = "unknown"
 )
 
+const help = `{{- if .Extended -}}
+{{ section "NAME" }}
+
+clockidup helps you generating your standup entry using the time entries from
+{{ url "https://clockify.me" }}.
+
+{{ end -}}
+{{ section "SYNOPSYS" }}
+
+    clockidup login
+    clockidup [--billable] {{ url "DAY" }}
+
+where {{ url "DAY" }} is of the form:
+
+    today
+    yesterday
+    thursday
+    "2 days ago"
+    2021-01-28
+
+{{- if .Extended }}
+
+{{ section "HOW TO USE IT" }}
+
+To start, log in:
+
+    {{ cmd "clockidup login" }}
+
+Each "project" in Clockify will show up as a prefix of your time entries. For
+example, imagining that you have an entry that you created with clockify-cli:
+
+    clockify-cli in prod/cert-manager --when=now "#3444: continue dataforcertificate unit test"
+    clockify-cli out
+
+where {{ url "prod/cert-manager" }} is a Clockify project. clockidup will show:
+
+    {{ cmd "clockidup today" }}
+    {{ out "Friday:" }}
+    {{ out "- [1.2] prod/cert-manager: #3444: continue dataforcertificate unit test" }}
+      <---> <--------------->  <------------------------------------------>
+    duration     project                        entry text
+
+You may only want the "billable" entries to be displayed by clockidup:
+
+    {{ cmd "clockidup --billable today" }}
+
+You can also use Clockify tasks. Like projects, there are displayed as a prefix
+to the entry text:
+
+    {{ out "- [1.2] prod/cert-manager: big refactoring for the v2: rm large defers" }}
+      <---> <--------------->  <------------------------> <-------------->
+     duration     project                 task               entry text
+
+{{ section "CONFIG FILE" }}
+
+The auth token is saved to {{ url "~/.config/standup.yml" }}. The file looks
+like this:
+
+    token: your-clockify-auth-token
+
+{{- end }}
+{{- if not .Extended }}
+
+More help is available with the command {{ yel "clockidup help" }}.
+
+{{- end }}
+
+{{ section "OPTIONS" }}
+
+`
+
 func main() {
-	flag.Parse()
-	logutil.EnableDebug = *debugFlag
+	printHelp := func(extended bool) func() {
+		return func() {
+			t, err := template.New("help").Funcs(map[string]interface{}{
+				"section": ansi.ColorFunc("black+hb"),
+				"url":     ansi.ColorFunc("white+u"),
+				"grey":    ansi.ColorFunc("white+d"),
+				"yel":     ansi.ColorFunc("yellow"),
+				"cmd": func(cmd string) string {
+					return ansi.ColorFunc("white+d")("% ") + ansi.ColorFunc("yellow+b")(cmd)
+				},
+				"out": ansi.ColorFunc("white+d"),
+			}).Parse(help)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-	flag.Usage = func() {
-		fmt.Fprint(os.Stderr, heredoc.Doc(`
-            Usage:
-              clockidup [options] (login | DATE)
-
-            Examples:
-              clockidup login
-              clockidup yesterday
-              clockidup today
-              clockidup thursday
-              clockidup "2 days ago"
-              clockidup 2021-01-28
-              clockidup --billable yesterday
-
-            Options:
-		`))
-		flag.PrintDefaults()
+			err = t.Execute(flag.CommandLine.Output(), struct{ Extended bool }{
+				Extended: extended,
+			})
+			if err != nil {
+				log.Fatal(err)
+			}
+			flag.PrintDefaults()
+		}
 	}
+	flag.Usage = printHelp(false)
+	flag.Parse()
 
-	err := Run(*tokenFlag)
+	err := Run(*tokenFlag, printHelp)
 	if err != nil {
 		logutil.Errorf(err.Error())
 		os.Exit(1)
 	}
 }
 
-func Run(tokenFlag string) error {
+func Run(tokenFlag string, printHelp func(bool) func()) error {
 	conf, err := loadConfig(confPath)
 	if err != nil {
 		return fmt.Errorf("could not load config: %s", err)
@@ -85,8 +165,11 @@ func Run(tokenFlag string) error {
 		}
 		logutil.Debugf("config: %+v", conf)
 		return nil
+	case "help":
+		printHelp(true)()
+		os.Exit(0)
 	case "":
-		flag.Usage()
+		printHelp(false)()
 		return fmt.Errorf("a command is required, e.g. 'login' or 'yesterday'")
 	default:
 		day, err = time.Parse(layoutISO, flag.Arg(0))
