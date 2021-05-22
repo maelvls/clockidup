8b93812bcef4ea555da3c2c3f16f768ea4695fed
Maël Valais <mael@vls.dev>
2021-05-22 10:32:32 +0200

    clockidup version now works even when installed with 'go install'

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
+	"regexp"
 	"strings"
 	"text/template"
 	"time"
@@ -27,14 +29,10 @@ var (
 	debugFlag    = flag.Bool("debug", false, "Show debug output, including the HTTP requests.")
 	onlyBillable = flag.Bool("billable", false, "Only print the entries that are billable.")
 
-	showVersion = flag.Bool("version", false, "Print version. Note that it returns 'n/a (commit none, built on unknown)' when built with 'go get'.")
 	// The 'version' var is set during build, using something like:
 	//  go build  -ldflags"-X main.version=$(git describe --tags)".
-	// Note: "version", "commit" and "date" are set automatically by
-	// goreleaser.
-	version = "n/a"
-	commit  = "none"
-	date    = "unknown"
+	// Note: "version", is set automatically by goreleaser.
+	version = ""
 )
 
 const help = `{{- if .Extended -}}
@@ -48,6 +46,7 @@ clockidup helps you generating your standup entry using the time entries from
 
     clockidup login
     clockidup [--billable] {{ url "DAY" }}
+    clockidup version
 
 where {{ url "DAY" }} is of the form:
 
@@ -168,6 +167,16 @@ func Run(tokenFlag string, printHelp func(bool) func()) error {
 			return fmt.Errorf("saving configuration: %s", err)
 		}
 		logutil.Debugf("config: %+v", conf)
+		return nil
+	case "version":
+		if version == "" {
+			version, err = versionUsingGo()
+			if err != nil {
+				return fmt.Errorf("binary not built with a version, and error while fetching the error using 'go version -m': %w", err)
+			}
+		}
+
+		fmt.Printf("%s\n", version)
 		return nil
 	case "help":
 		printHelp(true)()
@@ -345,4 +354,52 @@ func Run(tokenFlag string, printHelp func(bool) func()) error {
 	}
 
 	return nil
+}
+
+// Use Go installed on the system to get the git tag of the running Go
+// executable by running:
+//
+//   go version -m /path/to/binary
+//
+// and by parsing the "mod" line. For example, we want to show "v0.3.0"
+// from the following:
+//
+//   /home/mvalais/go/bin/clockidup: go1.16.3
+//      path    github.com/maelvls/clockidup
+//      mod     github.com/maelvls/clockidup      v0.3.0  h1:84sL4RRZKsyJgSs8KFyE6ykSjtNk79bBVa0ZgC48Kpw=
+//      dep     github.com/AlecAivazis/survey/v2  v2.2.12 h1:5a07y93zA6SZ09gOa9wLVLznF5zTJMQ+pJ3cZK4IuO8=
+func versionUsingGo() (string, error) {
+	bin, err := os.Executable()
+	if err != nil {
+		return "", fmt.Errorf("while trying to find the clockidup binary path: %s", err)
+	}
+	cmd := exec.Command("go", "version", "-m", bin)
+
+	bytes, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("while slurping stdout from 'go version -m %s': %s", bin, err)
+	}
+
+	if cmd.ProcessState.ExitCode() != 0 {
+		return "", fmt.Errorf("running 'go version -m %s': %s", bin, err)
+	}
+
+	// We want to be parsing the following line:
+	//
+	//       mod     github.com/maelvls/clockidup     v0.3.0  h1:84sL4RRZKsyJgSs8KFyE6ykSjtNk79bBVa0ZgC48Kpw=
+	//   <-->   <-->                             <---><---->
+	//   tab    tab                               tab  m[0][1]
+
+	regStr := `mod\s*[^\s]*\s*([^\s]*)`
+	//                        <------>
+	//                         m[0][1]
+
+	reg := regexp.MustCompile(regStr)
+
+	m := reg.FindAllStringSubmatch(string(bytes), 1)
+	if len(m) < 1 || len(m[0]) < 2 {
+		return "", fmt.Errorf("'go version -m %s' did not return a string of the form '%s':\nmatches: %v\nstdout: %s", bin, regStr, m, string(bytes))
+	}
+
+	return m[0][1], nil
 }
